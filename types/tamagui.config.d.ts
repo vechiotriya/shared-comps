a5f56823152a9f42cb6cfd7c2f9c1f1b87518f02
Riya Vechiot <vechiotriya@gmail.com>
2025-11-10 13:45:12 +0530

    refactor: update package.json and add Button component with type definitions

---
+++ types/tamagui.config.d.ts
@@ -0,0 +1,15 @@
+export declare const config: import("tamagui").TamaguiInternalConfig<{
+    color: {
+        background: string;
+        borderColor: string;
+    };
+}, {
+    light: {
+        background: string;
+        borderColor: string;
+    };
+}, {}, {}, {}, {}, {
+    allowedStyleValues: false;
+    autocompleteSpecificTokens: "except-special";
+}>;
+//# sourceMappingURL=tamagui.config.d.ts.map
