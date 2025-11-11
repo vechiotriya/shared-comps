72c1e10cbc512bb6a5b865982b10e2f9d1144f24
Riya Vechiot <vechiotriya@gmail.com>
2025-11-11 16:54:07 +0530

    refactor: update tamagui configuration to enhance token structure and theme definitions

--- dist/esm/tamagui.config.js
+++ dist/esm/tamagui.config.js
@@ -2,16 +2,47 @@ import { createTamagui } from "tamagui";
 const config = createTamagui({
   tokens: {
     color: {
+      white: "#fff",
+      black: "#000",
+      gray: "#ddd",
       background: "#fff",
-      borderColor: "#ddd"
+      border: "#ddd"
+    },
+    size: {
+      1: 4,
+      2: 8,
+      3: 12,
+      4: 16,
+      5: 20
+    },
+    space: {
+      1: 4,
+      2: 8,
+      3: 12,
+      4: 16,
+      5: 20
+    },
+    radius: {
+      0: 0,
+      1: 2,
+      2: 4,
+      3: 8,
+      4: 12
     }
   },
   themes: {
     light: {
       background: "#fff",
-      borderColor: "#ddd"
+      borderColor: "#ddd",
+      color: "#000"
+    },
+    dark: {
+      background: "#000",
+      borderColor: "#555",
+      color: "#fff"
     }
-  }
+  },
+  defaultTheme: "light"
 });
 var tamagui_config_default = config;
 export {
--- types/tamagui.config.d.ts
+++ types/tamagui.config.d.ts
@@ -1,12 +1,42 @@
 declare const config: import("tamagui").TamaguiInternalConfig<{
     color: {
+        white: string;
+        black: string;
+        gray: string;
         background: string;
-        borderColor: string;
+        border: string;
+    };
+    size: {
+        1: number;
+        2: number;
+        3: number;
+        4: number;
+        5: number;
+    };
+    space: {
+        1: number;
+        2: number;
+        3: number;
+        4: number;
+        5: number;
+    };
+    radius: {
+        0: number;
+        1: number;
+        2: number;
+        3: number;
+        4: number;
     };
 }, {
     light: {
         background: string;
         borderColor: string;
+        color: string;
+    };
+    dark: {
+        background: string;
+        borderColor: string;
+        color: string;
     };
 }, {}, {}, {}, {}, {
     allowedStyleValues: false;
